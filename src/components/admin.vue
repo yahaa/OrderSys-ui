36ad3d494a408477ed56d765de36f34dad261e46
yahaa <1477765176@qq.com>
2017-11-04 20:24:51 +0800

    完成管理员模块

--- src/components/admin.vue
+++ src/components/admin.vue
@@ -74,11 +74,53 @@
       </el-row>
 
       <el-row type="flex" class="row-bg" style="padding:0px 60px 0px 60px;">
+        <el-col :span="5">
+          <h3>销售员及其销售产品</h3>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20" style="padding:0px 60px 0px 60px;">
+        <el-col :span="10">
+          <el-table :data="salers">
+            <el-table-column label="销售员ID" width="180">
+              <template slot-scope="scope">
+                <span style="margin-left: 10px">{{ scope.row.userId }}</span>
+              </template>
+            </el-table-column>
+
+            <el-table-column label="销售产品ID" width="180">
+              <template slot-scope="scope">
+                <span style="margin-left: 10px">{{ scope.row.proId }}</span>
+              </template>
+            </el-table-column>
+
+            <el-table-column label="备注" width="180">
+              <template slot-scope="scope">
+                <el-tag size="medium" type="danger">{{ scope.row.marks }}</el-tag>
+              </template>
+            </el-table-column>
+
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button
+                  size="mini"
+                  type="danger"
+                  @click="delSaler(scope.$index, scope.row)" round>删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+
+        </el-col>
+        <el-col :span="12">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+      </el-row>
+
+      <el-row type="flex" class="row-bg" style="padding:0px 60px 0px 60px;">
         <el-col :span="2">
           <h3>产品管理</h3>
         </el-col>
         <el-col :span="2">
-          <el-button type="success" size="small" round @click="dialogFormVisible = true" style="margin-top:20px">添加产品</el-button>
+          <el-button type="success" size="small" round @click="addProduct()" style="margin-top:20px">添加产品</el-button>
         </el-col>
       </el-row>
       <el-row style="padding:0px 60px 0px 60px;">
@@ -136,13 +178,12 @@
             <el-button
               size="mini"
               type="danger"
-              @click="mkOrder(scope.$index, scope.row)" round>指派销售员</el-button>
+              @click="addSaler(scope.$index, scope.row)" round>指派销售员</el-button>
           </template>
             </el-table-column>
           </el-table>
         </el-col>
       </el-row>
-
 
     </el-main>
   </el-container>
@@ -215,6 +256,71 @@
     </div>
   </el-dialog>
 
+
+  <el-dialog title="指派销售任务" :visible.sync="salerTask">
+    <el-form :model="curTask">
+      <el-form-item label="商品ID" :label-width="formLabelWidth">
+        <el-input v-model="curTask.proId" size="10" :disabled="true"></el-input>
+      </el-form-item>
+
+      <el-form-item label="销售员ID" :label-width="formLabelWidth">
+        <el-select v-model="curTask.userId" placeholder="请选择">
+          <el-option v-for="item in users" v-if="item.roleId==='2'" :key="item.userId" :label="item.userId" :value="item.userId">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="备注" :label-width="formLabelWidth">
+        <el-input v-model="curTask.marks" size="10" placeholder="请输入备注"></el-input>
+      </el-form-item>
+
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button type="danger" @click="salerTask = false">取 消</el-button>
+      <el-button type="primary" @click="doAddSaler()">确 定</el-button>
+    </div>
+  </el-dialog>
+
+  <el-dialog title="添加加商品" :visible.sync="addProVisible">
+    <el-form :model="curAddPro">
+      <el-form-item label="商品ID" :label-width="formLabelWidth">
+        <el-input v-model="curAddPro.proId" size="10"></el-input>
+      </el-form-item>
+
+      <el-form-item label="商品名称" :label-width="formLabelWidth">
+        <el-input v-model="curAddPro.proName" size="10"></el-input>
+      </el-form-item>
+
+      <el-form-item label="商品模型" :label-width="formLabelWidth">
+        <el-input v-model="curAddPro.proSM" size="10"></el-input>
+      </el-form-item>
+
+      <el-form-item label="供应商家" :label-width="formLabelWidth">
+        <el-input v-model="curAddPro.proCom" size="10"></el-input>
+      </el-form-item>
+
+      <el-form-item label="商品单价" :label-width="formLabelWidth">
+        <el-input v-model="curAddPro.proPrice" size="10"></el-input>
+      </el-form-item>
+
+      <el-form-item label="采购总量" :label-width="formLabelWidth">
+        <el-input v-model="curAddPro.proCounts" size="10"></el-input>
+      </el-form-item>
+
+      <el-form-item label="商品款式" :label-width="formLabelWidth">
+        <el-input v-model="curAddPro.proStye" size="10"></el-input>
+      </el-form-item>
+
+      <el-form-item label="备注" :label-width="formLabelWidth">
+        <el-input v-model="curAddPro.marks" size="10" placeholder="请输入备注"></el-input>
+      </el-form-item>
+
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button type="danger" @click="addProVisible = false">取 消</el-button>
+      <el-button type="primary" @click="doAddProduct()">确 定</el-button>
+    </div>
+  </el-dialog>
+
 </div>
 </template>
 
@@ -222,6 +328,16 @@
 export default {
   data () {
     return {
+      salers: [{
+        'userId': '14122261',
+        'proId': '1005',
+        'marks': 'ggggggg'
+      }],
+      curTask: {
+        userId: '',
+        proId: '',
+        marks: ''
+      },
       products: [{
         'salerId': '',
         'salerName': '',
@@ -245,6 +361,8 @@ export default {
       }],
       dialogFormVisible: false,
       editerDia: false,
+      salerTask: false,
+      addProVisible: false,
       addUser: {
         'userId': '',
         'name': '',
@@ -276,15 +394,66 @@ export default {
         'sex': '',
         'phone': '',
         'roleId': ''
+      },
+      curAddPro: {
+        'proId': '',
+        'proName': '',
+        'proSM': '',
+        'proCom': '',
+        'proPrice': '',
+        'proCounts': '',
+        'proStye': '',
+        'marks': 'not thing too bad'
       }
     }
   },
   methods: {
+    addProduct () {
+      this.addProVisible = true
+    },
+    doAddProduct () {
+      let tvm = this
+      tvm.axios.defaults.headers.common['Authorization'] = localStorage.getItem('Token')
+      tvm.axios.post('/api/admin/product', tvm.curAddPro)
+        .then(function (res) {
+          tvm.addProVisible = false
+          tvm.getPros()
+          tvm.open()
+        }).catch(function (res) {
+          console.log(res)
+        })
+    },
     getPros () {
       let tvm = this
       this.axios.get('/api/admin/products')
         .then(function (res) {
           tvm.products = res.data
+        }).catch(function (res) {
+          console.log(res)
+        })
+    },
+    addSaler (index, row) {
+      let tvm = this
+      tvm.curTask.proId = row.proId
+      tvm.salerTask = true
+    },
+    doAddSaler () {
+      let tvm = this
+      tvm.axios.defaults.headers.common['Authorization'] = localStorage.getItem('Token')
+      tvm.axios.post('/api/admin/saler', tvm.curTask)
+        .then(function (res) {
+          tvm.salerTask = false
+          tvm.getSalers()
+          tvm.open()
+        }).catch(function (res) {
+          console.log(res)
+        })
+    },
+    getSalers () {
+      let tvm = this
+      this.axios.get('/api/admin/salers')
+        .then(function (res) {
+          tvm.salers = res.data
         }).catch(function (res) {
           console.log(res)
         })
@@ -317,7 +486,7 @@ export default {
     open () {
       const h = this.$createElement
       this.$notify({
-        title: '添加用户结果',
+        title: '添加结果',
         message: h('i', {
           style: 'color: teal'
         }, '添加成功 !')
@@ -338,6 +507,32 @@ export default {
               message: '删除成功!'
             })
             tvm.users.splice(index, 1)
+          }).catch(function (res) {
+            console.log(res)
+          })
+      }).catch(() => {
+        tvm.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
+
+    delSaler (index, row) {
+      let tvm = this
+      tvm.$confirm('此操作将永久撤销该销售员的销售任务, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        tvm.axios.defaults.headers.common['Authorization'] = localStorage.getItem('Token')
+        tvm.axios.put(`/api/admin/saler`, row)
+          .then(function (res) {
+            tvm.$message({
+              type: 'success',
+              message: '删除成功!'
+            })
+            tvm.salers.splice(index, 1)
           }).catch(function (res) {
             console.log(res)
           })
@@ -398,6 +593,7 @@ export default {
   mounted () {
     this.getUsers()
     this.getPros()
+    this.getSalers()
   }
 
 }
